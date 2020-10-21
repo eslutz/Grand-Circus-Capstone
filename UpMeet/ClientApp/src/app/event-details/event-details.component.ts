343b35327678ff8577b643cec43a5e2e8f75a8b7
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-10-20 20:41:04 -0400

    Added all the Angular components.

---
+++ UpMeet/ClientApp/src/app/event-details/event-details.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-event-details',
+    templateUrl: './event-details.component.html',
+    styleUrls: ['./event-details.component.css']
+})
+/** event-details component*/
+export class EventDetailsComponent {
+    /** event-details ctor */
+    constructor() {
+
+    }
+}
---
+++ UpMeet/ClientApp/src/app/events/events.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-events',
+    templateUrl: './events.component.html',
+    styleUrls: ['./events.component.css']
+})
+/** events component*/
+export class EventsComponent {
+    /** events ctor */
+    constructor() {
+
+    }
+}
